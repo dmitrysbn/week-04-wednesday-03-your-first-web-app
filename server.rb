fdb6cf0ff9eb930e33e6e08f6e86e72b8506af79
dmitrysbn <dmitry.sbn@gmail.com>
2018-02-14 19:51:25 -0500

    Added / and /gallery redirects

--- server.rb
+++ server.rb
@@ -18,3 +18,11 @@ get '/favourites' do
   @fav_links = ['http://www.imdb.com', 'http://www.nytimes.com']
   erb :fav_links
 end
+
+get '/' do
+  redirect to('/home')
+end
+
+get '/gallery' do
+  redirect to('/portfolio')
+end
