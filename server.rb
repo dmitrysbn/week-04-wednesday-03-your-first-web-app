86f85ec4a4b821b9c6b2d088805332eec2855716
dmitrysbn <dmitry.sbn@gmail.com>
2018-02-14 19:49:02 -0500

    Added fav_links view

--- server.rb
+++ server.rb
@@ -9,5 +9,12 @@ get '/portfolio' do
 end
 
 get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'cycling', 'techno', 'film']
   erb :about_me
+end
+
+get '/favourites' do
+  @fav_links = ['http://www.imdb.com', 'http://www.nytimes.com']
+  erb :fav_links
 end
